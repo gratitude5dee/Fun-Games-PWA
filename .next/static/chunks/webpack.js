889e7b4beb6815dc4f5c5c8a05aaa309dc847ecd
GRATITUD3.ETH <gratitude@5-dee.com>
2025-12-15 12:52:04 -0800

    Updated build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "67539098bc1f5c45"; }
+/******/ 		__webpack_require__.h = function() { return "604c27517fd6afcf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
